056ee7c57043c5525e0a051f759385ff9eae2e6c
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-08-10 18:18:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // write js code here if required
+ const value = document.getElementById('timer')
+function CurrentTime() {
+
+let date = new Date();
+	
+let day =date.getDay();
+let month = date.getMonth() +1;
+let year = date.getFullYear();
+let hour = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+}
+value.textContent = `${day}/${month}/${year},
+${hour}:${minutes}:${seconds}`;
+}
+setInterval(CurrentTime ,1000)
